0baa4a2685964ff3f636c9292bd6148916543d6a
Hindjanati <100366241+Hindjanati@users.noreply.github.com>
2022-10-22 10:30:03 +0100

    added http library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,6 +16,7 @@ import { PokemonModule } from './pokemon/pokemon.module';
   imports: [
     BrowserModule,
     FormsModule,
+    HttpClientModule,
     PokemonModule,
     AppRoutingModule
   ],
--- src/app/pokemon/list-pokemon/list-pokemon.component.ts
+++ src/app/pokemon/list-pokemon/list-pokemon.component.ts
@@ -18,6 +18,6 @@ export class ListPokemonComponent implements OnInit {
    }
   
   goToPokemon(pokemon: Pokemon){
-    this.router.navigate(['/pokemon', pokemon.id])
+    this.router.navigate(['/pokemons', pokemon.id])
   }
 }
